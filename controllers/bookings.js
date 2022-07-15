625780c0bc97a1b11ba29ce6213c4ffbbfde98ac
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-07-15 15:36:41 +0700

    add booking controller to create booking document from form in one.hbs

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,11 +1,18 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+const Bookings = require('../models/bookings')
 
 //Requests
 // POST ROUTE
 router.post('/', async (req, res, next) => {
   if (req.isAuthenticated()) {
+    let booking = await Bookings.create({
+      author: req.user._id,
+      description: req.body.description,
+      house: req.body.house
+    })
+    res.redirect(`/houses/${req.body.house}`)
   } else {
     res.redirect('/auth/login')
   }
